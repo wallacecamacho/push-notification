9c5652149d08946d6cf164a40f3f30004e972645
wallace c <wallacecamacho@gmail.com>
2020-11-27 14:36:50 -0300

    asd

--- firebase-messaging-sw.js
+++ firebase-messaging-sw.js
@@ -1,6 +1,19 @@
 importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
 importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
-importScripts('../src/firebase-init');
+
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC-IbMlaRCpe9ywQ2Ts2YmLGCDOQtyZZa8",
+  authDomain: "headless-12f0d.firebaseapp.com",
+  databaseURL: "https://headless-12f0d.firebaseio.com",
+  projectId: "headless-12f0d",
+  storageBucket: "headless-12f0d.appspot.com",
+  messagingSenderId: "186890154052",
+  appId: "1:186890154052:web:395c9e8f14a4643f"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
@@ -15,4 +28,9 @@ messaging.onBackgroundMessage(function(payload) {
 
   self.registration.showNotification(notificationTitle,
     notificationOptions);
+});
+
+messaging.onMessage((payload) => {
+  console.log('Message received. ', payload);
+  // ...
 });
