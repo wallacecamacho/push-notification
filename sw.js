34ef28963993988201b6931efa39185d6654f035
wallace c <wallacecamacho@gmail.com>
2020-11-27 11:51:55 -0300

    asd

--- sw.js
+++ sw.js
@@ -1,28 +1,39 @@
-function receivePushNotification(event) {
-    console.log("[Service Worker] Push Received.");
-  
-    const { image, tag, url, title, text } = event.data.json();
-  
-    const options = {
-      data: url,
-      body: text,
-      icon: image,
-      vibrate: [200, 100, 200],
-      tag: tag,
-      image: image,
-      badge: "https://spyna.it/icons/favicon.ico",
-      actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
-    };
-    event.waitUntil(self.registration.showNotification(title, options));
-  }
-  
-  function openPushNotification(event) {
-    console.log("[Service Worker] Notification click Received.", event.notification.data);
-    
-    event.notification.close();
-    event.waitUntil(clients.openWindow(event.notification.data));
-  }
-  
-  self.addEventListener("push", receivePushNotification);
-  self.addEventListener("notificationclick", openPushNotification);
+
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-app.js');
+importScripts('https://www.gstatic.com/firebasejs/8.1.1/firebase-messaging.js');
+
+var firebaseConfig = {
+  apiKey: "AIzaSyC-IbMlaRCpe9ywQ2Ts2YmLGCDOQtyZZa8",
+  authDomain: "headless-12f0d.firebaseapp.com",
+  databaseURL: "https://headless-12f0d.firebaseio.com",
+  projectId: "headless-12f0d",
+  storageBucket: "headless-12f0d.appspot.com",
+  messagingSenderId: "186890154052",
+  appId: "1:186890154052:web:395c9e8f14a4643f"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+const messaging = firebase.messaging();
+
+messaging.onBackgroundMessage(function(payload) {
+  console.log('[firebase-messaging-sw.js] Received background message ', payload);
+  // Customize notification here
+  const notificationTitle = 'Background Message Title';
+  const notificationOptions = {
+    data: url,
+    body: text,
+    icon: image,
+    vibrate: [200, 100, 200],
+    tag: tag,
+    image: image,
+    badge: "https://spyna.it/icons/favicon.ico",
+    actions: [{ action: "Detail", title: "View", icon: "https://via.placeholder.com/128/ff0000" }]
+  };
+
+  self.registration.showNotification(notificationTitle,
+    notificationOptions);
+});
+
+
   
